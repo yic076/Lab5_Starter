b944e224392b8e65d817c910c5adef15dda7f740
yic076 <yic076@ucsd.edu>
2022-10-29 21:26:05 -0700

    update

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,44 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const speechSynth = window.speechSynthesis;
+  let voices;
+  let voiceList = document.querySelector("textarea+select");
+  let talk = document.querySelector("button");
+  let textBox = document.querySelector("textarea");
+  let face = document.querySelector("img");
+  let utterance
+
+  speechSynthesis.addEventListener("voiceschanged", function(){
+    voices = speechSynth.getVoices();
+    for(let i = 0; i < voices.length; i++){
+      let opt = document.createElement("option");
+      opt.value = voices[i].name;
+      opt.text = voices[i].name;
+      voiceList.add(opt);
+    }
+  });
+
+  talk.addEventListener("click", function(){
+    utterance = new SpeechSynthesisUtterance(textBox.value);
+    utterance.voice = voices.filter(function(voice){
+      return voice.name == voiceList.value;
+    })[0];
+    speechSynthesis.speak(utterance);
+    face.src = "assets/images/smiling-open.png";
+    face.alt = "Smiling open face";
+  });
+
+  setInterval(function(){
+    if(textBox.value != "" && speechSynth.speaking){
+      face.src = "assets/images/smiling-open.png";
+      face.alt = "Smiling face";
+    }else{ 
+      face.src = "assets/images/smiling.png";
+      face.alt = "Smiling face";
+    }
+  });
 }
+
+
+
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,42 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const jsConfetti = new JSConfetti();
+
+  let hornList = document.getElementById("horn-select");
+  let hornImage = document.querySelector("header + img");
+  hornList.addEventListener('change', function(){
+    hornImage.src = "assets/images/" + hornList.value + ".svg";
+  });
+
+  let playSound = document.querySelector("button");
+  let hornSound = document.querySelector("audio");
+  playSound.addEventListener('click',function(){
+    if(hornList.value != "select"){
+      hornSound.src = "assets/audio/" + hornList.value + ".mp3";
+      hornSound.play();
+    }
+    if(hornList.value == "party-horn")
+      jsConfetti.addConfetti();
+  });
+
+  let volume = document.querySelector("#volume-controls input");
+  let volImage = document.querySelector("#volume-controls img");
+  volume.addEventListener('input', function(){
+    let value = volume.value;
+    hornSound.volume = value / 100;
+    if(value == 0){
+      volImage.src = "assets/icons/volume-level-0.svg";
+      volImage.alt = "Volume level 0";
+    } else if(value < 33){
+      volImage.src = "assets/icons/volume-level-1.svg";
+      volImage.alt = "Volume level 1";
+    } else if(value < 67){
+      volImage.src = "assets/icons/volume-level-2.svg";
+      volImage.alt = "Volume level 2";
+    } else{
+      volImage.src = "assets/icons/volume-level-3.svg";
+      volImage.alt = "Volume level 3";
+    }
+  });
 }
